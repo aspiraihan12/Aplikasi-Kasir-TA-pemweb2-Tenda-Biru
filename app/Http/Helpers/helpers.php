292c4b8fc11716f7b94fcbed1795110e4ef5446d
AlfikaNurfadia <alfikanurfadia@gmail.com>
2023-06-03 13:09:59 +0800

    layout dll

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -30,4 +30,28 @@ function terbilang($angka) {
         $terbilang = terbilang($angka/1000000000000) . " trilyun" . terbilang(fmod($angka,1000000000000));
     }     
     return $terbilang;
+};
+
+function tanggal_indonesia($tgl, $tampil_hari = true ) {
+    $nama_hari = array(
+        'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'
+    );
+    $nama_bulan = array(1 =>
+        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    );
+
+    $tahun = substr($tgl, 0, 4);
+    $bulan = $nama_bulan[(int) substr($tgl, 5, 2)];
+    $tanggal = substr($tgl, 8, 2);
+    $text = '';
+
+    if($tampil_hari) {
+        $urutan_hari = date('w', mktime(0, 0, 0, substr($tgl, 5, 2), $tanggal, $tahun));
+        $hari = $nama_hari[$urutan_hari];
+        $text .= "$hari, $tanggal $bulan $tahun";
+    } else {
+        $text .= "$tanggal $bulan $tahun";
+    }
+
+    return $text;
 }
